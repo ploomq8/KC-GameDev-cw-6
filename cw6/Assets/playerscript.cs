0c2c7220812145e197d6300823eb4928cb31116c
plom <plom.q8@gmail.com>
2022-08-16 19:24:48 +0300

    cw6

---
+++ cw6/Assets/playerscript.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerscript : MonoBehaviour
+{
+    public Transform max;
+    public Transform min;
+    public float inc;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        move();
+    }
+    void move()
+    {
+        if(Input.GetKeyDown(KeyCode.D))
+        {
+            transform.position = new Vector3(Mathf.Clamp(transform.position.x + inc, min.position.x, max.position.x), transform.position.y, transform.position.z); 
+        }
+       else if (Input.GetKeyDown(KeyCode.A))
+        {
+            transform.position = new Vector3(Mathf.Clamp(transform.position.x - inc, min.position.x, max.position.x), transform.position.y, transform.position.z);
+        }
+    }
+}
